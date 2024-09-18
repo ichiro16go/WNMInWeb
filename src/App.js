07269db7b950670ad838d4d79758f2c0beb58199
tanakamoe <m2216050tm@ug.jwu.ac.jp>
2024-09-18 16:48:33 +0900

    aaa

--- src/App.js
+++ src/App.js
@@ -27,6 +27,9 @@ function Header() {
       <h1>{pageTitle}</h1>
       </div>
       <div className='nikukyu'>
+        <div className='circle1'></div>
+        <div className='circle2'></div>
+        <div className='circle3'></div>
       </div>
     </header>
   );
