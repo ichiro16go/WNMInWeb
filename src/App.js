566d3273c74997a62ec20551207043e20c9bae2b
ichiro16go <ichi1.9adgjm@gmail.com>
2024-09-18 10:02:26 +0900

    add header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import { IoWalk, IoMap, IoBook } from 'react-icons/io5';
 import BookMarkPage from './view/pages/bmPage';
 import MapPage from './view/pages/mapPage';
 import WalkCoursePage from './view/pages/walkPage';
 import './App.css';
 
+function Header() {
+  const location = useLocation();
+  let pageTitle = '';
+
+  switch (location.pathname) {
+    case '/walk':
+      pageTitle = 'Walk Course';
+      break;
+    case '/map':
+      pageTitle = 'Map';
+      break;
+    case '/book':
+      pageTitle = 'Book Mark';
+      break;
+    default:
+      pageTitle = 'Home';
+  }
+
+  return (
+    <header className="header">
+      <h1>{pageTitle}</h1>
+    </header>
+  );
+}
+
 function App() {
   return (
     <Router>
       <div className="app">
+        <Header />
         <div className="content">
           <Routes>
             <Route path="/walk" element={<WalkCoursePage />} />
@@ -21,17 +47,17 @@ function App() {
           <ul>
             <li>
               <Link to="/walk">
-                <IoWalk /> Walk
+                <IoWalk /> 
               </Link>
             </li>
             <li>
               <Link to="/map">
-                <IoMap /> Map
+                <IoMap /> 
               </Link>
             </li>
             <li>
               <Link to="/book">
-                <IoBook /> Book
+                <IoBook /> 
               </Link>
             </li>
           </ul>
